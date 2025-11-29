a9d7ad286d70653e9a43343bed94ea69ddd1563a
Ashrith Puppala <ashrithpuppala07@gmail.com>
2025-11-30 00:48:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: './',
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  publicDir: 'public',
 })
